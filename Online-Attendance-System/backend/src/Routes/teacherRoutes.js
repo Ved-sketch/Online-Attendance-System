07908b92b6ecb7f4fb91825e0d79066f60e5b16d
Ved-sketch <vedgupta1806@gmail.com>
2026-02-04 10:27:26 +0530

    corrected the deleteStudent route

--- Online-Attendance-System/backend/src/Routes/teacherRoutes.js
+++ Online-Attendance-System/backend/src/Routes/teacherRoutes.js
@@ -7,7 +7,7 @@ router.get("/teacher/attendance",checkAttendance);
 router.get("/teacher/check_stats",showStats);
 router.get("/teacher/:id",showStudent);
 router.post("/teacher/add_student",addStudent);
-router.delete("/teacher/delete_student",deleteStudent);
+router.delete("/teacher/:id",deleteStudent);
 router.get("/teacher/download_stats",downloadStats);
 router.get("/teacher/sort",sortStudents);
 router.get("/teacher/showSchedule",showSchedule);
